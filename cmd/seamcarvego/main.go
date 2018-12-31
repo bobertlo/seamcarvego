772e5d3b01b49cc578b8fd79cb8066c4df2b32ff
Robert Lowry <bobertlo@gmail.com>
2018-12-30 23:04:43 -0600

    first version of frontend

--- cmd/seamcarvego/main.go
+++ cmd/seamcarvego/main.go
@@ -1,4 +1,69 @@
 package main
 
+import (
+	"fmt"
+	"github.com/bobertlo/seamcarvego/internal/carver"
+	"image"
+	_ "image/png"
+	"os"
+	"strconv"
+)
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: seamcarvego [input] [output] [cols] [rows]")
+}
+
 func main() {
+	if len(os.Args) != 5 {
+		usage()
+		os.Exit(1)
+	}
+
+	// parse dimension arguments
+	cols, err := strconv.Atoi(os.Args[3])
+	rows, err2 := strconv.Atoi(os.Args[4])
+	if err != nil || err2 != nil {
+		fmt.Fprintln(os.Stderr, "invalid dimension arguments")
+		usage()
+		os.Exit(1)
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to open %s: %s\n", os.Args[1], err)
+		os.Exit(1)
+	}
+	img, _, err := image.Decode(f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to decode %s: %s\n", os.Args[1], err)
+		os.Exit(1)
+	}
+
+	if rows > img.Bounds().Dy() || cols > img.Bounds().Dx() {
+		fmt.Println("invalid target dimensions")
+		os.Exit(1)
+	}
+
+	c, err := carver.NewArrayCarver(img)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error initializing carver: %s\n", err)
+		os.Exit(1)
+	}
+
+	for c.Width() > cols || c.Height() > rows {
+		if c.Height() > rows {
+			s, err := c.HSeam()
+			if err != nil {
+				panic("carver error")
+			}
+			c.HRemoveSeam(s)
+		}
+		if c.Width() > cols {
+			s, err := c.VSeam()
+			if err != nil {
+				panic("carver error")
+			}
+			c.VRemoveSeam(s)
+		}
+	}
 }
