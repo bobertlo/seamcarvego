ba155d95f3a9e5f8ddead5925c7cb9fa39456f3a
Robert Lowry <bobertlo@gmail.com>
2019-01-13 02:04:56 -0600

    formatting

--- cmd/seamcarvego/main.go
+++ cmd/seamcarvego/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/bobertlo/seamcarvego/internal/carver"
 	"image"
 	"image/png"
 	"os"
 	"strconv"
+
+	"github.com/bobertlo/seamcarvego/internal/carver"
 )
 
 func usage() {
